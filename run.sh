c8fb2d2bdafca75fce316d72b95907dacc792cd8
tudv <dovantu1996@gmail.com>
2019-10-11 10:26:48 +0700

    add imageupload

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SERVER=$(curl ifconfig.co)
-JMX_PORT=22001
+JMX_PORT=22002
 
 CALMOB_HOME=$(dirname "$0")
 JMX_PARAM=" -Djava.rmi.server.hostname=$SERVER -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
@@ -8,5 +8,5 @@ DEBUG_PARAM="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5555 
 echo $JMX_PARAM
 cd "$CALMOB_HOME"
 
-java -Xmx4096M -Xms312M $JMX_PARAM -cp conf/:target/*:target/libs/*:libs/* com.ttc.faceid.ImageManagerServiceMain "$@" &
+java -Xmx4096M -Xms312M $JMX_PARAM -cp conf/:target/*:target/libs/*:libs/*:src/main/resources/ com.ttc.faceid.ImageManagerServiceMain "$@" &
 echo $! >pid
